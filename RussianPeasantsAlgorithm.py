8cbe25e9db21ed9519ea8936b2daadaafd33143e
Luko van der Maas <lukovandermaas@gmail.com>
2015-04-05 10:43:15 +0200

    finished the algorithm and made it binary

---
+++ RussianPeasantsAlgorithm.py
@@ -0,0 +1,9 @@
+import time
+
+def russian_peasant_rec_bin(x, y):
+    if x == 1:
+        return y
+    elif x % 2 == 0:
+        return russian_peasant_rec_bin(x >> 1, y << 1)
+    else:
+        return russian_peasant_rec_bin(int(x >> 1), y << 1) + y
