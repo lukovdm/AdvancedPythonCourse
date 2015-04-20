8d0208ec32d74eed7a1025a245e0023ac3532130
Luko van der Maas <lukovandermasa@gmail.com>
2015-04-20 13:09:19 +0200

    added a test function for the russian algoritm

--- RussianPeasantsAlgorithm.py
+++ RussianPeasantsAlgorithm.py
@@ -1,9 +1,18 @@
 import time
 
-def russian_peasant_rec_bin(x, y):
+def russian_peasant_rec(x, y):
     if x == 1:
         return y
     elif x % 2 == 0:
-        return russian_peasant_rec_bin(x >> 1, y << 1)
+        return russian_peasant_rec(x >> 1, y << 1)
     else:
-        return russian_peasant_rec_bin(x >> 1, y << 1) + y
+        return russian_peasant_rec(x >> 1, y << 1) + y
+
+def test_russian():
+    t = time.time()
+    print russian_peasant_rec(357, 16)
+    print "russian paeasant algoritm took %f seconds." % (time.time()-t)
+    assert russian_peasant_rec(357, 16) == 5712
+
+if __name__ == "__main__":
+    test_russian()
